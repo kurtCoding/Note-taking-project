7476d875cf375dfc73ebb9c7a14b9dbe559ae6e4
Antonio <anthonymartz17@hotmail.com>
2024-05-05 15:25:06 -0400

    implemented creating,updateing and deleting in backend

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,24 @@ import SideBar from "./components/SideBar";
 import { getNotesList, addNewNote } from "./services/notesApi.js";
 import { useEffect, useState } from "react";
 
-
 function App() {
   const [notes, setNotes] = useState([]);
 
-  console.log(notes)
+  console.log(notes);
   function getNotes() {
     getNotesList()
       .then((data) => {
         setNotes([...data]);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   }
 
   useEffect(() => {
     getNotes();
   }, []);
-  
+
   return (
     <div className="container">
       <div className="sidebar">
