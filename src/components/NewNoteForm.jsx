4f1a9e2b1642b053a6b57f8e5dfb0fd42e44dfa4
kurtCoding <kjulien7790@gmail.com>
2024-05-05 11:40:20 -0400

    added, add newNote

--- src/components/NewNoteForm.jsx
+++ src/components/NewNoteForm.jsx
@@ -1,7 +1,28 @@
 import {} from 'react'
 
 export default function NewNoteForm() {
+
   return (
-    <div>NewNoteForm</div>
+    <div>
+      <h1>New Note</h1>
+      <br/>
+      <form className="newNoteForm">
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name='title' />
+        <br />
+        <label htmlFor="noteArea">Scribe Here..</label>
+        <br />
+        <textarea id='noteArea' className='noteArea' rows="20" cols="70">Eureka!!! I shouted, as I raced to my notepad to jot down my thoughts...</textarea>
+        <br />
+        <label htmlFor="categories">NoteBank</label>
+        <select name="categories" id="categories">Remember Me?
+        <option value="workNotes">Work Notes</option>
+        <option value="people?">People?</option>
+        <option value="random">Random</option>
+        <option value="allNotes">All Notes</option>
+        </select>
+        <input type="submit" value="Add Note" className="submitButton" />
+      </form>
+    </div>
   )
 }
