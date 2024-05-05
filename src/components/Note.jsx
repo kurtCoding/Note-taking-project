effa4828e9d9160f76b5711bc2def7a60929c151
gerardjr42 <ggarciajr12@gmail.com>
2024-05-05 12:45:49 -0400

    fix category icon

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -13,9 +13,9 @@ export default function Note({ note }) {
   return(
   <div className="mb-3 ml-auto mr-auto w-3/4 rounded px-1 py-6 text-start border-b">
       <div className="flex items-center">
-        <span className={`inline-block rounded-full w-3 h-3 ${dot}`}></span>
           <h2 className="ml-2 font-bold text-font">{note.title}</h2>
         <span className="ml-auto text-xs font-normal text-accent">{note.category.toUpperCase()}</span>
+        <span className={`ml-2 inline-block rounded-full w-3 h-3 ${dot}`}></span>
       </div>
       <br />
       <h3 className="text-font ml-4">{note.body}</h3>
