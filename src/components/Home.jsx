a63ca8a2eb9af775b79f9f0b71c657feef6a8e4f
gerardjr42 <ggarciajr12@gmail.com>
2024-05-05 17:31:51 -0400

    style: new note

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <section className="over fixed right-0 h-screen w-[85%] overflow-y-auto scroll-smooth bg-primary">
+    <section className="fixed right-0 h-screen w-[85%] overflow-y-auto scroll-smooth bg-primary">
       <h1 className="text-font text-center">Hello World</h1>
     </section>
   )
--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,53 +1,65 @@
-import { useState } from 'react'
-import { addNewNote } from "../services/notesApi.js"
+import { useState } from "react";
+import { addNewNote } from "../services/notesApi.js";
 
 export default function NewNote() {
-
   const [newNote, setNewNote] = useState({
     title: "",
     body: "",
-    category: ""
+    category: "",
   });
 
   function handleSubmit(event) {
-    event.preventDefault()
-    addNewNote(newNote).then((res) => {
-      
-    }) 
+    event.preventDefault();
+    addNewNote(newNote).then((res) => {});
   }
 
   function handleTextChange(event) {
     setNewNote({
       ...newNote,
       [event.target.id]: event.target.value,
-    })
+    });
   }
 
   return (
-    <section className="over fixed right-0 h-screen w-[85%] overflow-y-auto scroll-smooth bg-primary">
-      <h1 className="text-font text-center">New Note Form</h1>
-  
-    <div>
-      <h1>New Note</h1>
-      <br/>
-      <form className="newNoteForm" onSubmit={handleSubmit}>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" name='title' />
+    <section className="overflow-y-auto scroll-smooth bg-primary">
+      <form
+        className="fixed right-0 flex h-screen w-[85%] flex-col pl-24 pr-24"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-4xl text-font text-center mt-6">New Note</h1>
+        <label htmlFor="title" className="text-xl text-white">
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          className="border border-[#878585] bg-transparent text-font mb-4 text-2xl"
+        />
+
+        <label htmlFor="noteArea" className="text-xl text-white">Scribe Here..</label>
+        <textarea
+          onSubmit={handleTextChange}
+          value={newNote.body}
+          id="noteArea"
+          className="border border-[#878585] bg-transparent text-font mb-4 text-base"
+          rows="20"
+          cols="70"
+        >
+          Eureka!!! I shouted, as I raced to my notepad to jot down my
+          thoughts...
+        </textarea>
         <br />
-        <label htmlFor="noteArea">Scribe Here..</label>
-        <br />
-        <textarea onSubmit={handleTextChange} value={newNote.body} id='noteArea' className='noteArea' rows="20" cols="70">Eureka!!! I shouted, as I raced to my notepad to jot down my thoughts...</textarea>
-        <br />
-        <label htmlFor="categories">NoteBank</label>
-        <select name="categories" id="categories">Remember Me?
-        <option value="workNotes">Work Notes</option>
-        <option value="people?">People?</option>
-        <option value="random">Random</option>
-        <option value="allNotes">All Notes</option>
+        <label htmlFor="categories" className="text-xl text-white">NoteBank</label>
+        <select name="categories" id="categories" className="border border-[#878585] bg-transparent text-font mb-4">
+          Remember Me?
+          <option value="workNotes">Work Notes</option>
+          <option value="people?">People?</option>
+          <option value="random">Random</option>
+          <option value="allNotes">All Notes</option>
         </select>
-        <input type="submit" value="Add Note" className="submitButton" />
+        <input type="submit" value="Add Note" className="w-20 text-center border border-[#878585] bg-transparent text-font mb-4" />
       </form>
-    </div>
     </section>
-  )
+  );
 }
