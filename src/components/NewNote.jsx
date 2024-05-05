08c5d8635449bcbe7d4873a12c5027e7ac95f873
kurtCoding <kjulien7790@gmail.com>
2024-05-05 17:45:59 -0400

    removed category, form variable categories

--- src/components/NewNote.jsx
+++ src/components/NewNote.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import { addNewNote } from "../services/notesApi.js"
 
+const categories = [
+  {name: "Work", id: "1"}, 
+  {name: "School", id: "1"}, 
+  {name: "Development", id: "1"}, 
+]
 export default function NewNote() {
 
   const [newNote, setNewNote] = useState({
@@ -14,6 +19,11 @@ export default function NewNote() {
     addNewNote(newNote).then((res) => {
       
     }) 
+    setNewNote({
+    title: "",
+    body: "",
+    category: ""
+  })
   }
 
   function handleTextChange(event) {
@@ -23,6 +33,10 @@ export default function NewNote() {
     })
   }
 
+  // function resetForm() {
+  //   setNewNote([...newNote])
+  // }
+
   return (
     <section className="over fixed right-0 h-screen w-[85%] overflow-y-auto scroll-smooth bg-primary">
       <h1 className="text-font text-center">New Note Form</h1>
@@ -30,22 +44,20 @@ export default function NewNote() {
     <div>
       <h1>New Note</h1>
       <br/>
-      <form className="newNoteForm" onSubmit={handleSubmit}>
+      <form className="fixed right-0 flex h-screen w-[85%] flex-col pl-12 pr-12" onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
-        <input type="text" id="title" name='title' />
+        <input onChange={handleTextChange} type="text" id="title" name='title' value={newNote.title} require />
         <br />
         <label htmlFor="noteArea">Scribe Here..</label>
         <br />
-        <textarea onSubmit={handleTextChange} value={newNote.body} id='noteArea' className='noteArea' rows="20" cols="70">Eureka!!! I shouted, as I raced to my notepad to jot down my thoughts...</textarea>
+        <textarea required onChange={handleTextChange} value={newNote.body} id='body' className='noteArea' rows="20" cols="70">Eureka!!! I shouted, as I raced to my notepad to jot down my thoughts...</textarea>
         <br />
         <label htmlFor="categories">NoteBank</label>
-        <select name="categories" id="categories">Remember Me?
-        <option value="workNotes">Work Notes</option>
-        <option value="people?">People?</option>
-        <option value="random">Random</option>
-        <option value="allNotes">All Notes</option>
+        <select onChange={handleTextChange} name="categories" id="category" value={newNote.category}>
+          <option value="" disabled >Select</option>
+        {categories.map((ele) => <option value={ele.name}>{ele.name}</option> )}
         </select>
-        <input type="submit" value="Add Note" className="submitButton" />
+        <input type="submit" value="Add Note" className="w-20 text-center border border-[#878585] bg-transparent text-font mb-4" />
       </form>
     </div>
     </section>
