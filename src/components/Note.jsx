d98f50d1f7609166ca23178b9ee39b27be00412f
gerardjr42 <ggarciajr12@gmail.com>
2024-05-05 12:51:13 -0400

    fix category icon

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,24 +1,28 @@
 export default function Note({ note }) {
   let dot;
   if (note.category === "school") {
-    dot = 'bg-green-400'
+    dot = "bg-green-400";
   }
   if (note.category === "work") {
-    dot = 'bg-purple-400'
+    dot = "bg-purple-400";
   }
   if (note.category === "development") {
-    dot = 'bg-[#FEA639]'
+    dot = "bg-[#FEA639]";
   }
 
-  return(
-  <div className="mb-3 ml-auto mr-auto w-3/4 rounded px-1 py-6 text-start border-b">
+  return (
+    <div className="mb-3 ml-auto mr-auto w-3/4 rounded border-b px-1 py-6 text-start">
       <div className="flex items-center">
-          <h2 className="ml-2 font-bold text-font">{note.title}</h2>
-        <span className="ml-auto text-xs font-normal text-accent">{note.category.toUpperCase()}</span>
-        <span className={`ml-2 inline-block rounded-full w-3 h-3 ${dot}`}></span>
+        <h2 className="text-font ml-2 font-bold">{note.title}</h2>
+        <span className="ml-auto text-xs font-normal text-accent">
+          {note.category.toUpperCase()}
+        </span>
+        <span
+          className={`ml-2 inline-block h-3 w-3 rounded-full ${dot}`}
+        ></span>
       </div>
       <br />
       <h3 className="text-font ml-4">{note.body}</h3>
-  </div>
+    </div>
   );
 }
