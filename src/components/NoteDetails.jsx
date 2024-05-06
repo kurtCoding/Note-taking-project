ef9316b24ab3347f7585bddbaead1e09ff8a76a6
Marcusb86 <Marcusb08@gmail.com>
2024-05-06 18:59:36 -0400

    details

--- src/components/NoteDetails.jsx
+++ src/components/NoteDetails.jsx
@@ -1,46 +1,66 @@
-import { useState, useEffect } from "react"
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import ErrorMessage from "../Errors/ErrorMessage";
-import { useParams, useNavigate } from "react-router-dom";
-// import { getNoteById } from "../api/fetch";
+import { deleteNote } from "../services/notesApi";
 
-export default function NoteDetails({notes, error}) {
+export default function NoteDetails({ notes }) {
   const { id } = useParams();
   const [note, setNote] = useState("");
-  const [loadingError, setLoadingError] = useState(false)
+  const [loadingError, setLoadingError] = useState(false);
   let navigate = useNavigate();
 
   useEffect(() => {
-
-    const gotNote = notes.find((n) => n.id === id)
-
-    console.log(gotNote)
-    if(gotNote) {
-      setNote(gotNote)
+    const gotNote = notes.find((n) => n.id === id);
+    console.log(gotNote);
+    if (gotNote) {
+      setNote(gotNote);
     } else {
-      setLoadingError(false)
+      setLoadingError(false);
     }
-  }, [])
+  }, [id, notes]);
+
+  console.log(note);
 
   function handleEdit() {
-    navigate(`/notes/${id}/edit`)
+    navigate(`/notes/${id}/edit`);
   }
 
-//   function handleDelete() {
-//     // onDelete(id);
-//     navigate("/notes");
-//   }
-  
+  function handleDelete(id) {
+    deleteNote(id);
+    setNote(null);
+    navigate("/notes")
+  }
+
   return (
-    <div>
-          <h2>Note Details</h2>
-          <p>ID: {note.id}</p>
-          <p>Title: {note.title}</p>
-          <p>Body: {note.body}</p>
-          <p>Category: {note.category}</p>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py px-1 rounded inline-flex items-center" onClick={handleEdit}>Edit</button>
-          <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py px-1 rounded inline-flex items-center"  onClick={handleDelete}>Delete</button>
-
-
-    </div>
+    <section className="over fixed right-0 h-screen w-[85%] overflow-y-auto scroll-smooth bg-primary text-center">
+      {loadingError ? (
+        <ErrorMessage />
+      ) : (
+        <>
+          <h1 className="mb-10 mt-10 text-center text-5xl text-fuchsia-200">
+            {note.title}
+            <span className="fixed right-0 mr-28 text-base text-accent">
+            <select name="x" id="x" className="bg-accent text-font cursor-pointer custom-dropdown mr-20 rounded-full">
+            <option value="x" hidden className="text-center">&nbsp;&nbsp; &hellip;</option>
+            <option
+              className="cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </option>
+            <option
+              className="cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </option>
+          </select>
+              {note.category}
+            </span>
+          </h1>
+          <h2 className="text-xl text-font mx-16">{note.body}</h2>
+        </>
+      )}
+    </section>
   );
 }
